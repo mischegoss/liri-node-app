dffe4a0a082176a2e90a00b704b6678f8dcfe6f9
Tamar Auber <mischegoss@gmail.com>
2019-03-23 23:32:46 -0400

    Spotify working

--- liri.js
+++ liri.js
@@ -1,5 +1,11 @@
 require("dotenv").config();
-/*var spotify = new Spotify(keys.spotify);*/
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify({
+    id: '88a18c33735646268279b54130d7cc64',
+    secret: 'b7cd943cf1f549198daf3d9abc352546'
+  });
+
+var inquirer = require('fs');
 
 /* This is the initial prompt to direct to which API */
 var inquirer = require('inquirer');
@@ -25,7 +31,8 @@ var askFirstPrompt = function() {
         console.log ('You picked one')
         break;
       case 'spotify-this-song':
-        console.log ('You picked two')
+        console.log ('Let\'s Find Your Song')
+        spotifyAction();
         break;
         case 'movie-this':
         console.log ('You picked two')
@@ -39,5 +46,31 @@ var askFirstPrompt = function() {
     }
   }
   );
+}
+
+function spotifyAction() {
+    inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'spot_song',
+            message: "What song should I find for you?",
+            filter: function(val) {
+                return val.toLowerCase();
+              }
+          }
+    ])
+    .then(function(answers) {
+        var mytrack = answers.spot_song
+        console.log(mytrack)
+        spotify.search({ type: 'track', query: mytrack}, function(err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            } 
+            console.log("Artist: " + data.tracks.items[0].artists[0].name + "\nSong name: " + data.tracks.items[0].name +
+            "\nAlbum Name: " + data.tracks.items[0].album.name + "\nPreview Link: " + data.tracks.items[0].preview_url); 
+            
+        });
+
+    })
 }
 askFirstPrompt();
